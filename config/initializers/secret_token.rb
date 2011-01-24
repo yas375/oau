aae3554697a73f0f6001eee6a58e4a51b56d36d0
yas <ilyukevich.victor@gmail.com>
2011-01-24 21:51:01 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KursachPoOau::Application.config.secret_token = 'f0022dfe5924dcce80ff441cce428ea8d227d7d21080980ed2bad581745903f6c8af96dad8f4cf51b74a5fbb5daf3f10a9d3f0f81d71ae92f5f5a8e21f6d3786'
